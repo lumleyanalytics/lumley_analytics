c419dc66f90c6218c97c3d11620558b5cead3e7f
apratsunrthd <apratsunrthd@gmail.com>
2024-10-13 13:59:31 -0400

    Changes to be committed: 	deleted:    compiled/lumley_analytics/models/chatt_police/base_chatt_police_police_incidents.sql 	deleted:    compiled/lumley_analytics/models/chatt_police/schema_chatt_police.yml/dbt_utils_source_unique_combin_3e05fc8bd4f4ca14d7021b3b20b02718.sql 	deleted:    compiled/lumley_analytics/models/chatt_police/schema_chatt_police.yml/dbt_utils_unique_combination_o_151bea3a931fa333e5802b322fe2162b.sql 	deleted:    compiled/lumley_analytics/models/example/my_first_dbt_model.sql 	deleted:    compiled/lumley_analytics/models/example/my_second_dbt_model.sql 	deleted:    compiled/lumley_analytics/models/noaa_lightning/base_noaa_lightning_lightning_strikes.sql 	deleted:    compiled/lumley_analytics/models/test/test_recurse/test_this.sql 	deleted:    compiled/lumley_analytics/models/test/test_this.sql 	modified:   models/chatt_police/marts/schema_chatt_police_marts.yml 	deleted:    run/lumley_analytics/models/chatt_police/base_chatt_police_police_incidents.sql 	deleted:    run/lumley_analytics/models/chatt_police/schema_chatt_police.yml/dbt_utils_source_unique_combin_3e05fc8bd4f4ca14d7021b3b20b02718.sql 	deleted:    run/lumley_analytics/models/chatt_police/schema_chatt_police.yml/dbt_utils_unique_combination_o_151bea3a931fa333e5802b322fe2162b.sql 	deleted:    run/lumley_analytics/models/example/my_first_dbt_model.sql 	deleted:    run/lumley_analytics/models/example/my_second_dbt_model.sql 	deleted:    run/lumley_analytics/models/noaa_lightning/base_noaa_lightning_lightning_strikes.sql 	deleted:    run/lumley_analytics/models/test/test_recurse/test_this.sql 	deleted:    run/lumley_analytics/models/test/test_this.sql

--- compiled/lumley_analytics/models/example/my_second_dbt_model.sql
+++
@@ -1,5 +0,0 @@
--- Use the `ref` function to select from other models
-
-select *
-from `lumley-analytics`.`dbt_dev`.`my_first_dbt_model`
-where id = 1
--- run/lumley_analytics/models/test/test_this.sql
+++
@@ -1,6 +0,0 @@
-
-
-  create or replace view `lumley-analytics`.`test`.`test_this`
-  OPTIONS()
-  as select 1 as rn, "This test worked" as slug;
-
