c06c6be6022845d55a1bb4c011a0a595ffa2247e
apratsunrthd <apratsunrthd@gmail.com>
2024-10-12 21:27:56 -0400

    fixed selection in model

--- models/noaa_lightning/base_noaa_lightning_lightning_strikes.sql
+++ models/noaa_lightning/base_noaa_lightning_lightning_strikes.sql
@@ -2,10 +2,10 @@ with source as (
       select {{ dbt_utils.star(source('noaa_lightning', 'lightning_strikes'))}} from {{ source('noaa_lightning', 'lightning_strikes') }}
 ),
 renamed as (
-    select
+    select *
         
 
     from source
 )
-select * from renamed limit 10
+select * from renamed
   
